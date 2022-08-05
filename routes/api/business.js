492ee0b5ff338eb1f4d29099ec8af4030bc281ca
raihan <mostafa.raihan@newscred.com>
2022-08-05 10:36:25 +0600

    add routing

--- routes/api/business.js
+++ routes/api/business.js
@@ -3,6 +3,7 @@ import business from "../../business/index.js";
 const createRoute = (app) => {
   app.get("/api/business", business.getAllBusiness);
   app.post("/api/business", business.saveBusiness);
+  app.delete("/api/business", business.deleteBusiness);
 };
 
 export default {
